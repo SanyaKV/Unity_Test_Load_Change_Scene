bcb8534b8a4eab42782c41cbd67722805113d10a
ks <s>
2025-09-08 17:27:39 +0300

    ...

--- Assets/Загрузка сцены/ЗагрузкаСцены.cs
+++ Assets/Загрузка сцены/ЗагрузкаСцены.cs
@@ -1,52 +1,79 @@
-using UnityEditor.VersionControl;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ЗагрузкаСцены : MonoBehaviour
 {
-    [SerializeField] private string LoadSceneName;
-    [SerializeField] private string MenuSceneName;
     [SerializeField] private Animator componentAnimator;
-    #region Сист
-    #region СинглТон
-    public static ЗагрузкаСцены Instance;//СинглТон (Singleton)
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else Destroy(gameObject);
-    }
-    #endregion //СинглТон
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().name != LoadSceneName)
-        {
-            SceneManager.LoadScene(LoadSceneName);
-        }
-    }
+    [SerializeField] private TextMeshProUGUI TextПрогресаЗагрузки;
+    [SerializeField] private Image ПрогресБарЗагрузки;
+
+    private AsyncOperation asyncOperation_ПрогрессЗагрузки;
+
+    public static ЗагрузкаСцены Instance;//СинглТон (Singleton)
+    public static bool ИгратьАнимациюПроявленияПриВыводеСцены = false;
+    private static string ПереключаемаяСцена;
+
     private void Start()
     {
+        Instance = this;
         if (componentAnimator == null)
             componentAnimator = GetComponent<Animator>();
+        if (ИгратьАнимациюПроявленияПриВыводеСцены)
+        {
+            ИгратьАнимациюПроявленияПриВыводеСцены = false;
+            Instance.componentAnimator.SetTrigger(name: "ЗагрузкаСцены_Конец");
+        }
     }
-    #endregion //Сист
-    private AsyncOperation asyncOperation_ПрогрессЗагрузки;
+    private float load_progress_cur = 0;
+    private void Update()
+    {
+        if (asyncOperation_ПрогрессЗагрузки != null)
+        {
+            float progress = asyncOperation_ПрогрессЗагрузки.progress;
+            if (asyncOperation_ПрогрессЗагрузки.isDone || progress >= 0.9f)
+                progress = 1;
+            load_progress_cur = Mathf.Lerp(load_progress_cur, progress, Time.deltaTime * 3);
+
+            TextПрогресаЗагрузки.text = Mathf.RoundToInt(load_progress_cur * 100) + "%";
+            ПрогресБарЗагрузки.fillAmount = load_progress_cur;
+            if (load_progress_cur >= 0.9f)
+            {
+                asyncOperation_ПрогрессЗагрузки.allowSceneActivation = true;
+                ИгратьАнимациюПроявленияПриВыводеСцены = true;
+            }
+            Debug.Log("ЗагрузкаСцены = "+ progress + "%");
+        }
+        else
+        {
+            TextПрогресаЗагрузки.text = "0%";
+            ПрогресБарЗагрузки.fillAmount = 0f;
+            load_progress_cur = 0f;
+        }
+    }
     public static void ПереключитьСценуАлинхронно(string sceneName)
     {
-        Instance.componentAnimator.SetTrigger(name: "SceneLoad_Open");
-        Instance.asyncOperation_ПрогрессЗагрузки =  SceneManager.LoadSceneAsync(sceneName);
-        Instance.asyncOperation_ПрогрессЗагрузки.allowSceneActivation = false;//После загрузки сцены сразу на ней не переключаться
-        //12:00
+        Instance.componentAnimator.SetTrigger(name: "ЗагрузкаСцены_Начало");//Установить тригер запуска анимации затемнения экрана
+        ПереключаемаяСцена = sceneName;
+
+        //Instance.asyncOperation_ПрогрессЗагрузки =  SceneManager.LoadSceneAsync(sceneName);
+        //Instance.asyncOperation_ПрогрессЗагрузки.allowSceneActivation = false; //После загрузки сцены сразу на неё не переключаться
+        
     }
-    public void GoToGame()
+    //Вызывается когда закончилась анимация появления окна загрузки
+    public void OnAnimatorOver()
     {
-        SceneManager.LoadScene(MenuSceneName);
+        asyncOperation_ПрогрессЗагрузки = SceneManager.LoadSceneAsync(ПереключаемаяСцена);
+        Debug.Log("ЗагрузкаСцены = " + asyncOperation_ПрогрессЗагрузки.progress + "%");
+        asyncOperation_ПрогрессЗагрузки.allowSceneActivation = false; //После загрузки сцены сразу на неё не переключаться
+
+        //Instance.asyncOperation_ПрогрессЗагрузки.allowSceneActivation = true;//Разрешить переключение сцены
     }
+
     public void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName);
     public void LoadScene(int sceneIndex) => SceneManager.LoadScene(sceneIndex);
 }
+
 
